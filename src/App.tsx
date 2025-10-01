a4ed3e53af90db2066fdcd4c20729bfe8984c100
Andriï Zakhartchenko <andriizakhartchenko@outlook.fr>
2025-10-01 16:30:49 +0200

    saved

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,26 @@ import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
 import { useState } from 'react';
-import { Movie } from './types/Movie'
+import { Movie } from './types/Movie';
 
 const initialMovie = moviesFromServer.map(movie => ({
-  ...movie 
-}))
+  ...movie,
+}));
+
 export const App = () => {
   const [movie, setMovie] = useState<Movie[]>(initialMovie);
-   
-  const addMovie = (newMovie: Movie) => {
-      setMovie(currentMovies => [...currentMovies, newMovie])
 
-  }
+  const addMovie = (newMovie: Movie) => {
+    setMovie(currentMovies => [...currentMovies, newMovie]);
+  };
 
   return (
     <div className="page">
-
       <div className="page-content">
         <MoviesList movies={movie} />
       </div>
       <div className="sidebar">
-        <NewMovie  onAdd={addMovie}  />
+        <NewMovie onAdd={addMovie} />
       </div>
     </div>
   );
